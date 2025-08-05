823401ecfebc7ec32db5dbf2c22a58834ab77b3e
Delinson25 <101377077+Delinson25@users.noreply.github.com>
2025-08-05 22:08:03 +0200

    Add files via upload

---
+++ esp32dev/src/wifi_station.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+
+#define WIFI_SSID "Dekis T"
+#define WIFI_PASS "dracula58"
+
+static const char *TAG = "wifi_station";
+
+void wifi_init_sta(void)
+{
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+}
+
+void app_main(void)
+{
+    nvs_flash_init();
+    wifi_init_sta();
+}
